94fa642e6a4c623b612d311f43fc68df4e85ccd0
tvallotton <tvallotton@uc.cl>
2021-05-13 19:35:37 -0400

    pene

---
+++ src/params.py
@@ -0,0 +1,14 @@
+
+
+
+B = set(range(0, 10))
+P = set(range(0, 1000))
+T = set(range(0, 12))
+
+# presupuesto maximo
+M = { t: 100_000_000 for t in T }
+
+cap = { t: MAX for b in B }
+
+
+stock_init
