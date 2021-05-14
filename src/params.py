33df46f895491e733a66344b9a99f5901c402626
tvallotton <tvallotton@uc.cl>
2021-05-13 20:16:03 -0400

    pico

--- src/params.py
+++ src/params.py
@@ -10,21 +10,26 @@ T = set(range(0, 12))
 # presupuesto maximo
 M = { t: 100_000_000 for t in T }
 
+# Capacidad de la bodega
 cap = { b: randint(0, 1000) for b in B }
+
+# fecha de expiración inical
 exp = { p: randint(6, 24) for p in P}
 
+# stock inicial
 stock_init = { (p, b, e): randint(0, 3000) 
     for p in P  
     for b in B
     for e in range(0, exp[p])
 }
 
-guarda = { (p, b): randint()
+
+guarda = { (p, b): randint(0, 1)
     for p in P
     for b in B
 }
 vol = { p: random()*10 for p in P }
-unit = {p: choice(12, 24, 50, 100) for p in P}
+unit = {p: choice([12, 24, 50, 100]) for p in P}
 costo = { p: randint(10_000, 400_000) for p in P}
 
 d = { (p, t): random() * 1000 
@@ -32,7 +37,7 @@ d = { (p, t): random() * 1000
     for t in T 
 }
 
-stock_crit = { (p, b): random(0, 1000)
+stock_crit = { (p, b): randint(0, 1000)
     for p in P
     for b in B
 }
